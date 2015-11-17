0bc0ba8b540ff1d54e1f60464332398fda4d3f90
davidyoakum <dyoakum@gmail.com>
2015-11-17 16:03:19 -0700

    what

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,3 +1,19 @@
 app.controller('HomeController', ['$scope', function($scope){
-  // $scope.welcome = "Lets Get Started!"
+  $scope.inputs = [{label: "choice.label1", value: 'choice.value1' }]
+  $scope.addField = function (){
+    var newItemNo = $scope.inputs.length+1;
+    $scope.inputs.push({label: "choice.label" + newItemNo, value: "choice.value"+newItemNo});
+  }
+  $scope.removeField = function (){
+    var lastItem = $scope.inputs.length-1;
+    $scope.inputs.splice(lastItem);
+  }
+  $scope.submit = function (){
+    $scope.labels = [],
+    $scope.values = []
+    $scope.inputs.forEach(function(input){
+      $scope.labels.push(input.label1)
+      $scope.values.push(input.value1)
+    })
+  }
 }])
