dcf0043695ca9c5bd01e005b9b6c54d7a32d9cc5
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-17 15:33:25 -0700

    working angular form w/ duplication

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,10 +1,38 @@
 app.controller('HomeController', ['$scope', function($scope){
-  $scope.submit = function (){
-    $scope.labels = []
-    $scope.values = []
-    $scope.labels.push(this.label1, this.label2, this.label3, this.label4, this.label5)
-    $scope.values.push(this.value1, this.value2, this.value3, this.value4, this.value5)
-    console.log($scope.labels)
-    console.log($scope.values)
+  $scope.inputs = [{label: "choice.label1", value: 'choice.value1' }]
+  $scope.addField = function (){
+    var newItemNo = $scope.inputs.length+1;
+    $scope.inputs.push({label: "choice.label" + newItemNo, value: "choice.value"+newItemNo});
+    console.log($scope.inputs);
+  }
+  $scope.removeField = function (){
+    var lastItem = $scope.inputs.length-1;
+    $scope.inputs.splice(lastItem);
   }
 }])
+  // $scope.submit = function (){
+  //   $scope.labels = []
+  //   $scope.values = []
+  //   $scope.labels.push(this.label1, this.label2, this.label3, this.label4, this.label5)
+  //   $scope.values.push(this.value1, this.value2, this.value3, this.value4, this.value5)
+  //   console.log($scope.labels)
+  //   console.log($scope.values)
+  // }
+
+// var app = angular.module('angularjs-starter', []);
+//
+//   app.controller('MainCtrl', function($scope) {
+//
+//   $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+//
+//   $scope.addNewChoice = function() {
+//     var newItemNo = $scope.choices.length+1;
+//     $scope.choices.push({'id':'choice'+newItemNo});
+//   };
+//
+//   $scope.removeChoice = function() {
+//     var lastItem = $scope.choices.length-1;
+//     $scope.choices.splice(lastItem);
+//   };
+//
+// });
