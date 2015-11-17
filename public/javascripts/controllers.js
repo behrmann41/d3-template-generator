83bc9854d46e8d8729ffbc1163f5f4fa6f0bbcd4
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-17 14:52:57 -0700

    form setup

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,3 +1,10 @@
 app.controller('HomeController', ['$scope', function($scope){
-  // $scope.welcome = "Lets Get Started!"
+  $scope.submit = function (){
+    $scope.labels = []
+    $scope.values = []
+    $scope.labels.push(this.label1, this.label2, this.label3, this.label4, this.label5)
+    $scope.values.push(this.value1, this.value2, this.value3, this.value4, this.value5)
+    console.log($scope.labels)
+    console.log($scope.values)
+  }
 }])
