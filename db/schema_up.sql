043e50b0ba135bcdf65da81c35c5b639d87aed56
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-19 11:29:47 -0700

    create new users in db with pw hash

--- db/schema_up.sql
+++ db/schema_up.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
   id serial primary key,
   email varchar(50),
   password varchar(100)
-)
+);
--- routes/users.js
+++ routes/users.js
@@ -6,54 +6,50 @@ var bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
-  
-  var errors = [];
-
-  var hash = bcrypt.hashSync(req.body.signupForm.password, 10);
-
-  if (!req.body.signupForm.email.trim()) {
-    errors.push("Please enter a valid email")
-  }
-
-  if (!req.body.signupForm.password.trim()) {
-    errors.push("Please enter a password")
-  }
-
-  if (req.body.signupForm.password.length < 8) {
-    errors.push("Password needs to be at least 8 characters")
-  }
-
-  if (!req.body.email.match(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i)){
-    errors.push('Invalid Email')
-  }
-
-  if (req.body.signupForm.password !== req.body.signupForm.confirmPassword) {
-    errors.push("Passwords do not match")
-  }
-
-  if (errors.length) {
-    
-  }
-
+  console.log(req.body)
+  var hash = bcrypt.hashSync(req.body.user.password, 10)
   pg.connect(conString, function(err, client, done) {
-
-
-
-
     if (err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('INSERT INTO users(shell, taste) VALUES($1, $2) returning id',[req.body.shell, req.body.taste], function(err, result) {
+    client.query('INSERT INTO users(email, password) VALUES($1, $2)',[req.body.user.email, hash], function(err, result) {
       done();
-      res.redirect('/tacos')
+      console.log(result, "RESULT")
+      res.status(200).json(result)
       if (err) {
         return console.error('error running query', err);
       }
       console.log("connected to database")
     });
-
   });
-
 });
 
 module.exports = router;
+
+// var errors = [];
+//
+// var hash = bcrypt.hashSync(req.body.signupForm.password, 10);
+//
+// if (!req.body.signupForm.email.trim()) {
+//   errors.push("Please enter a valid email")
+// }
+//
+// if (!req.body.signupForm.password.trim()) {
+//   errors.push("Please enter a password")
+// }
+//
+// if (req.body.signupForm.password.length < 8) {
+//   errors.push("Password needs to be at least 8 characters")
+// }
+//
+// if (!req.body.email.match(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i)){
+//   errors.push('Invalid Email')
+// }
+//
+// if (req.body.signupForm.password !== req.body.signupForm.confirmPassword) {
+//   errors.push("Passwords do not match")
+// }
+//
+// if (errors.length) {
+//
+// }
